bf9352b79e1f129ad08731a27862e57337c204b0
Muhammadaziz <muxammadazizramziddinov@gmail.com>
2023-07-25 10:46:59 +0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const app = express()
 
 connectToDB()
 
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors({ origin: 'https://admin.megamix.getter.uz', credentials: true }));
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
